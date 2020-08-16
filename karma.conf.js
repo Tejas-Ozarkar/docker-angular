8b898a167d5ef9efa729baa1f71e016bbc9b2b23
Tejas-Ozarkar <tejozarkar@gmail.com>
2020-08-16 20:24:51 +0530

    Headless Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-process.env.CHROME_BIN = require('puppeteer').executablePath()
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -27,6 +25,17 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222',
+        ]
+      }
+    },
     browsers: ['ChromeHeadless'],
     singleRun: true,
     restartOnFileChange: false
